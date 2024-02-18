5a24f3efc9caad281aa4764fffcaaec439734cdf
mkamburdev <muhammed.kambur@dci-student.org>
2024-02-18 03:06:47 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,69 @@
+const karakterler =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+
+function olustur(e) {
+  e.preventDefault();
+
+  const sifreUzunlugu = parseInt(
+    document.getElementById("sifreUzunlugu").value
+  );
+  let sifre = "";
+  for (let i = 0; i < sifreUzunlugu; i++) {
+    const rastgeleIndex = Math.floor(Math.random() * karakterler.length);
+    sifre += karakterler[rastgeleIndex];
+  }
+  document.getElementById("sifre").textContent = sifre;
+  hesaplaGuc(sifre);
+
+  history.pushState({}, "", "/index.html");
+}
+
+function kopyala() {
+  const sifre = document.getElementById("sifre").textContent;
+  navigator.clipboard.writeText(sifre);
+  alert("Passwort kopiert!");
+}
+
+function hesaplaGuc(sifre) {
+  let guc = 0;
+  const kucukHarfler = "abcdefghijklmnopqrstuvwxyz";
+  const buyukHarfler = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const rakamlar = "0123456789";
+  const ozelKarakterler = "!@#$%^&*()";
+
+  for (let i = 0; i < sifre.length; i++) {
+    if (kucukHarfler.includes(sifre[i])) {
+      guc++;
+    } else if (buyukHarfler.includes(sifre[i])) {
+      guc++;
+    } else if (rakamlar.includes(sifre[i])) {
+      guc++;
+    } else if (ozelKarakterler.includes(sifre[i])) {
+      guc++;
+    }
+  }
+
+  if (sifre.length >= 12) {
+    guc++;
+  }
+
+  const gucGostergesi = document.getElementById("guc-gostergesi");
+  if (guc <= 3) {
+    gucGostergesi.textContent = "Die Passwortstärke ist zu niedrig!";
+    gucGostergesi.style.color = "red";
+  } else if (guc <= 6) {
+    gucGostergesi.textContent = "Die Passwortstärke ist mittelmäßig.";
+    gucGostergesi.style.color = "orange";
+  } else {
+    gucGostergesi.textContent = "Die Passwortstärke ist stark!";
+    gucGostergesi.style.color = "green";
+  }
+}
+
+window.onload = function () {
+  var elements = document.body.getElementsByTagName("*");
+
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].classList.add("animate");
+  }
+};
